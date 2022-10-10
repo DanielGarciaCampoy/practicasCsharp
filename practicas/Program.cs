a5f14031131fff6f266aed2d9ad2b9f9ad40e8d5
Daniel García Campoy <daniel.garcia.campoy@gmail.com>
2022-10-10 13:19:52 +0200

    ej1 hecho

--- practicas/Program.cs
+++ practicas/Program.cs
@@ -11,7 +11,12 @@ namespace practicas
         static void Main(string[] args)
         {
 
-
+            //ej1
+            System.Console.Write("Introduzca la medida del radio: ");
+            double radio = Double.Parse(Console.ReadLine());
+            double pi = 3.14;
+            double area = Math.Pow(radio, 2) * pi;
+            Console.WriteLine($"El área del circulo con radio {radio} es: {area}");
 
         }
     }
